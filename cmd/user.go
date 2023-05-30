1c3bcf7a080c71a771958403635c78b08dd3020c
Lucas Crownover <lcrownover127@gmail.com>
2023-05-29 19:26:50 -0700

    user doc

--- cmd/user.go
+++ cmd/user.go
@@ -13,13 +13,12 @@ import (
 // userCmd represents the user command
 var userCmd = &cobra.Command{
 	Use:   "user",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Manage HPC users",
+	Long: `User objects are the basis of HPC management.
+There are three role a user can serve for a PIRG:
+1. PI: This is the person who is responsible for the project, and the owner of the PIRG.
+2. Admin: This is the person who is capable of the day-to-day management of the PIRG.
+3. Member: This is the person who is a member of the PIRG, and has access to the PIRG's resources.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("user called")
 	},
